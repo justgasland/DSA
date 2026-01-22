49dbc6c60dc9434890d348c58e2f09aed23d00ca
justgasland <abdulgaffarsonola@gmail.com>
2026-01-22 02:43:27 +0100

    solved a dsa today

---
+++ Practice/indexnum.py
@@ -0,0 +1,16 @@
+#User function Template for python3
+class Solution:
+    def subarraySum(self, arr, target):
+        left=0
+        add=0
+        for i in range(0, len(arr)):
+            add +=arr[i]
+            
+            while add> target:
+                add-=arr[left]
+                left +=1 
+                
+            if add == target:
+                return [left+1 , i +1]
+        return [-1]
+            
